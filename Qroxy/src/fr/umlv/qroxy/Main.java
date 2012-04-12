030f92e8ddf70e1855cd09ba90f7cfbe285d0935
joan.goyeau <joan.goyeau@30a5e7ce-d178-9078-707f-7268befb07af>
2012-04-12 15:23:30 +0000

    Base

---
+++ Qroxy/src/fr/umlv/qroxy/Main.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2012 Joan Goyeau & Guillaume Demurger
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package fr.umlv.qroxy;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+
+/**
+ *
+ * @author joan
+ */
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        ServerSocketChannel server = ServerSocketChannel.open();
+        
+        server.socket().bind(new InetSocketAddress("127.0.0.1", 7777));
+        
+        SocketChannel socket = server.accept();
+    }
+}
