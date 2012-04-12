c1ee4735bdcd5ca50c6b8ebf91afbaa799185454
joan.goyeau <joan.goyeau@30a5e7ce-d178-9078-707f-7268befb07af>
2012-04-12 12:33:15 +0000

    Hey !

---
+++ trunk/Qroxy/src/fr/umlv/qroxy/config/Config.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.umlv.qroxy.config;
+
+/**
+ *
+ * @author joan
+ */
+public class Config {
+    
+}
---
+++ trunk/Qroxy/src/fr/umlv/qroxy/config/web/Server.java
@@ -0,0 +1,4 @@
+package fr.umlv.qroxy.config.web;
+
+public class Server {
+}
