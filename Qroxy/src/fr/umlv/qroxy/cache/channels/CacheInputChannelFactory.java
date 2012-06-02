9f4a9e2c09960c59db80336202c80351f36940ae
gdemurger <gdemurger@30a5e7ce-d178-9078-707f-7268befb07af>
2012-06-02 00:11:29 +0000

    bla

--- Qroxy/src/fr/umlv/qroxy/cache/channels/CacheInputChannelFactory.java
+++ Qroxy/src/fr/umlv/qroxy/cache/channels/CacheInputChannelFactory.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package fr.umlv.qroxy.cache;
+package fr.umlv.qroxy.cache.channels;
 
 import java.io.IOException;
 import java.nio.file.Path;
