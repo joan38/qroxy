eefe79cc55610b0c2be6146ec4087cd33c570ba8
joan.goyeau <joan.goyeau@30a5e7ce-d178-9078-707f-7268befb07af>
2012-06-06 11:41:50 +0000

    Mr Propre

--- trunk/Qroxy/src/fr/umlv/qroxy/proxy/HttpConnectionHandler.java
+++ trunk/Qroxy/src/fr/umlv/qroxy/proxy/HttpConnectionHandler.java
@@ -273,7 +273,6 @@ public class HttpConnectionHandler implements LinkHandler, Delayed {
                 }
                 // Activate speed limit juste by puting something not null
                 bytesLeftInSecond = 0;
-                System.out.println("Qos activated");
             } catch (NullPointerException e) {
                 // No QosRule
             }
